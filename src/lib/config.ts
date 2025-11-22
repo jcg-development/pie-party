587a62a67024124aabcf55c3cbfcc6767b6d3e40
jamesgardner <jimmycgardner@gmail.com>
2025-11-22 11:40:46 -0800

    Update pie type caps to 12 sweet and 12 savory

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -3,8 +3,8 @@ export type Category = typeof CATEGORIES[number]
 
 // RSVP Pie Type Caps
 export const PIE_TYPE_CAPS = {
-  sweet: 15,   // Maximum number of sweet pies allowed
-  savory: 10   // Maximum number of savory pies allowed
+  sweet: 12,   // Maximum number of sweet pies allowed
+  savory: 12   // Maximum number of savory pies allowed
 } as const
 
 export type PieType = 'sweet' | 'savory'
